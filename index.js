07c50765b18d933322ec4d5ad2457c3b507c6b21
Steven McVey <smcvey1210@gmail.com>
2022-08-27 21:51:54 -0400

    commit tree 0e611a784cbd04ba271ad33c4d7d148b66c21d61

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i<array.length; i++){
+    const before = array.slice(0, i)
+    const after = array.slice(i+1)
+    const diff = target - array[i]
+
+    debugger;
+    if (before.includes(diff) || after.includes(diff)){
+      return true
+    }
+  }
+  return false
 }
+// [22, 19, 4, 6, 30], 25
 
 /* 
   Write the Big O time complexity of your function here
